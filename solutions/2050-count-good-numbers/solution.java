42048291c344c0b2cb449b6473dcd3a4bf79821e
Saloni Shaw <saloni6289@gmail.com>
2026-05-22 04:54:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.9 MB (87.47%)

---
+++ solutions/2050-count-good-numbers/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    long MOD=1000000007;
+    public int countGoodNumbers(long n) {
+        long evenCount=(n+1)/2;
+        long oddCount=n/2;
+        long evenWays=power(5,evenCount);
+        long oddWays=power(4,oddCount);
+        return (int)((evenWays*oddWays)%MOD);
+       } 
+       public long power(long base,long exp){
+        if(exp==0){
+            return 1;
+        }
+        long half=power(base,exp/2);
+        long result=(half*half)%MOD;
+        if(exp%2==1){
+            result=(result*base)%MOD;
+        }
+        return result;
+       }
+}
