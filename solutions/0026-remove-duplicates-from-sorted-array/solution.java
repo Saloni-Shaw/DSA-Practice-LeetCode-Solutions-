09a04fb8a834cc9d0272fe3e3841adca2b04db46
Saloni Shaw <saloni6289@gmail.com>
2026-05-27 05:38:26 +0000

    Sync LeetCode submission Runtime - 1 ms (77.79%), Memory - 46.5 MB (80.40%)

--- solutions/0026-remove-duplicates-from-sorted-array/solution.java
+++ solutions/0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,12 +1,19 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int k=1;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]!=nums[i-1]){
-                nums[k]=nums[i];
-                k+=1;
+        int left=0;
+        int right=left+1;
+        int count=1;
+        while(right<nums.length){
+            if(nums[left]==nums[right]){
+                right++;
+            }
+            else if (nums[left]!=nums[right]){
+                left++;
+                nums[left]=nums[right];
+                right++;
+                count++;
             }
         }
-        return k;
+        return count;
     }
 }
