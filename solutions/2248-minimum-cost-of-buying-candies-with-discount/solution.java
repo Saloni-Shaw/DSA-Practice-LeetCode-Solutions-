85c76eea6b83403a006f0507e4860bc54fec5603
Saloni Shaw <saloni6289@gmail.com>
2026-06-09 05:53:42 +0000

    Sync LeetCode submission Runtime - 5 ms (95.43%), Memory - 44.6 MB (52.84%)

---
+++ solutions/2248-minimum-cost-of-buying-candies-with-discount/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+       Arrays.sort(cost);
+       int min=0;
+       int count=0;
+       for(int i=cost.length-1;i>=0;i--){
+        count++;
+        if(count%3==0){
+            continue;
+        }
+        else{
+            min+=cost[i];
+        }
+       }
+       return min;
+    }
+}
