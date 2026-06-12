6a7ac2841db743feafa35c30798349e98f1bf611
Saloni Shaw <saloni6289@gmail.com>
2026-06-12 06:23:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 45 MB (57.83%)

---
+++ solutions/2106-find-greatest-common-divisor-of-array/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findGCD(int[] nums) {
+       int min=nums[0];
+       int max=nums[0];
+       for(int i=0;i<nums.length;i++){
+        if(nums[i]<min){
+            min=nums[i];
+        }
+        if(nums[i]>max){
+            max=nums[i];
+        }
+       }
+       return gcd(min,max);
+    }
+       private int gcd(int a, int b){
+        while(b!=0){
+            int remainder=a%b;
+            a=b;
+            b=remainder;
+        }
+       return a;
+    }
+}
